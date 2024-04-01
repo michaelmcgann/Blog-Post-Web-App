723f3a849dc958368dc1cc5862f22c4919ae50de
Mike <126602126+michaelmcgann@users.noreply.github.com>
2024-04-01 14:43:04 +0100

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import { v4 } from "uuid";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const posts = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
